c73b7d206ef2dc0d8e825d989608fbaea67a84dc
Bignarem <35050327+Bignarem@users.noreply.github.com>
2026-03-01 13:56:07 -0400

    refactor: Remove unused imports

--- app/src/main/java/com/cnes/unitconverter/MainActivity.kt
+++ app/src/main/java/com/cnes/unitconverter/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.cnes.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -23,7 +22,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cnes.unitconverter.ui.theme.UnitConverterTheme
